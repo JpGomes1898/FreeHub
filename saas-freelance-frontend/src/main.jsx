ad47a01f548d86100da9a9cceab8d28a34d311bd
João Pedro Gomes <jp@MacBook-Air-de-Joao.local>
2026-01-08 00:45:28 -0300

    Muda para HashRouter para corrigir erro 404

--- saas-freelance-frontend/src/main.jsx
+++ saas-freelance-frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 1. IMPORTANTE: Mude de BrowserRouter para HashRouter
+import { HashRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. Envolva o App com HashRouter */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 )
